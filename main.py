064f9a8741990453cecc3cf0035f61e3bb39efca
Gleb <glebpetrof2007@gmail.com>
2024-11-12 12:21:29 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,12 +1,12 @@
-a=str(input('Введите 1 строку '))
+a=str(input('Введите 1 строку '))#Ввод двух строк
 b=str(input('Введите 2 строку '))
 c=False
-if(len(a)==len(b)):
-    for i in range(len(a)):
+if(len(a)==len(b)):#проверка равенства длины строк
+    for i in range(len(a)):#проверка на аннаграмму
         if(a[i] in b):
             c=True
         i+=1
-if(c):
+if(c):                            #вывод
     print('Строчки аннаграмы')
 if(c==False):
     print('Строки разные')
