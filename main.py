970ab7261bb83b6e02359895215379821878d11b
Gleb <glebpetrof2007@gmail.com>
2024-12-12 12:46:34 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,11 +1,13 @@
 string1 = input('Введите 1 строку ')#Ввод двух строк
 string2 = input('Введите 2 строку ')
-parametr = False
-if len(string1)==len(string2):#проверка равенства длины строк
+parametr = True
+if(len(string1)==len(string2)):#проверка равенства длины строк
     for i in range(len(string2)):#проверка на аннаграмму
         if string1[i] == string2[len(string2)-i-1]:
-            parametr=True
-if parametr:                            #вывод
+            pass
+        else:
+            parametr=False
+if(parametr):                            #вывод
     print('Строчки аннаграмы')
 else:
     print('Строки разные')
