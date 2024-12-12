cd96a3026d84356816dd9f1f48e5437b76293480
Gleb <glebpetrof2007@gmail.com>
2024-12-12 12:52:08 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 string1 = input('Введите 1 строку ')#Ввод двух строк
 string2 = input('Введите 2 строку ')
 parametr = True
-if(len(string1)==len(string2)):#проверка равенства длины строк
+if len(string1)==len(string2):#проверка равенства длины строк
     for i in range(len(string2)):#проверка на аннаграмму
         if string1[i] == string2[len(string2)-i-1]:
             pass
